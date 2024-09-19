ebffe4520b74d99ce4eda8ed5fb0fafcfd7ec308
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-09-18 21:49:16 -0300

    18/09/24

---
+++ JavaProjectsNoite/exemplo_conteudo_basico/src/main/java/com/example/Exercicio3Revisao.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Exercicio3Revisao {
+    //atributos
+    double valor1 = 0;
+    double valor2 = 0;
+    double resultado = 0;
+
+    int operacao = 0;
+
+    public double soma(double a, double b){
+        this.resultado = resultado a+b;
+        return resultado;
+    }
+    public double subtrair(double a, double b){
+        this.resultado = resultado a-b;
+        return resultado;
+    }
+    public double multiplicar(double a, double b){
+        this.resultado = resultado a*b;
+        return resultado;
+    }
+    public double dividir(double a, double b){
+        this.resultado = resultado a/b;
+    }catch (Exception e) {
+        
+    }
+        return resultado;
+    }
+        
+}
--- JavaProjectsNoite/exemplo_conteudo_basico/src/main/java/com/example/Main.java
+++ JavaProjectsNoite/exemplo_conteudo_basico/src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        new Exercicio3();
+       new Exercicio1Revisao().calculoMedia();
     }
 }
