0cbcf1a4083e965758bcbb1141140cde58195c0b
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-09-20 20:59:15 -0300

    20/09/24

---
+++ JavaProjectsNoite/exercicio2_gerenciar_funcionarios/src/main/java/main/java/com/example/Funcionario.java
@@ -0,0 +1,22 @@
+package main.java.com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class Funcionario {
+    //Atributos
+    private String nome;
+    private int idade;
+    private double salario;
+ 
+    //toString
+    @Override
+    public String toString() {
+       return "Nome: " + nome + ", idade: " + idade +", salario: " + salario;
+}
+};
---
+++ JavaProjectsNoite/exercicio2_gerenciar_funcionarios/src/main/java/main/java/com/example/FuncionarioController.java
@@ -0,0 +1,12 @@
+package main.java.com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuncionarioController {
+    private List<Funcionario> funcionarios;
+
+    public FuncionarioController() {
+        funcionarios = new ArrayList<>();
+    }
+}
