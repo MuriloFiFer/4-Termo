3498415a78681389715df4361ce9bc2ee5dbd642
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-09-30 21:49:19 -0300

    30/09/24

---
+++ JavaProjectsNoite/io_exemplo/src/main/java/com/example/LeituraAPI.java
@@ -0,0 +1,17 @@
+package main.java.com.example;
+
+import java.net.HttpURLConnection;
+
+public class LeituraAPI {
+    public void exemplo() {
+        try {
+            
+            URL url = new URL("https://api.github.com/users/MuriloFiFer");
+            HttpURLConnection con = new HttpURLConnection(url) {
+
+            };
+        } catch (Exception e) {
+    
+    }
+}
+};
