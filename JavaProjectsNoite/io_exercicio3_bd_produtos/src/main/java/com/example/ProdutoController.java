6b2aa40a4c7f246dc11a5eaa75f6f288de4da58e
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-10-02 21:53:16 -0300

    02/10/24

---
+++ JavaProjectsNoite/io_exercicio3_bd_produtos/src/main/java/com/example/ProdutoController.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class ProdutoController {
+    private list<Produtos> produtos;
+    
+    public ProdutoController() {
+        produtos = new ArrayList<>();
+    }
+}
