52605f0583c89802f10effbb6a411ff5b95fcc7b
MuriloFiFer <murilo.fischer.senai23@outlook.com>
2024-10-09 20:19:06 -0300

    0910124

--- JavaProjectsNoite/somativa_java/src/main/java/com/example/view/MaquinasPanel.java
+++ JavaProjectsNoite/somativa_java/src/main/java/com/example/view/MaquinasPanel.java
@@ -3,15 +3,22 @@ package com.example.view;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JButton;
-import javax.swing.table.DefaulTableModel;
+import javax.swing.table.DefaultTableModel;
+import java.awt.BorderLayout;
 
 import com.example.controllers.MaquinaController;
 
 public class MaquinasPanel extends JPanel {
     private MaquinaController maquinaController;
     private JTable maquinasTable;
-    private DefaulTableModel tableModel;
+    private DefaultTableModel tableModel;
     private JButton btnSalvarAlteracoes;
     private JButton btnCadastrarMauqina;
+
+    //construtor
+    public MaquinasPanel() {
+        super(new BorderLayout());
+        maquinaController = new MaquinaController();
+}
 
 }
